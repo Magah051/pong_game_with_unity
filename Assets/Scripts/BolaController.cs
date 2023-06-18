f7e2628a18a35ecb41d96c1d09bc06ead2dd5ac0
José Magalhães <joses.magalhaes@hotmail.com>
2023-06-18 12:48:04 -0300

    Upload do game

---
+++ Assets/Scripts/BolaController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BolaController : MonoBehaviour
+{
+    //Criando a variável para saber quem é o meu rigidbody
+    public Rigidbody2D meuRB;
+    private Vector2 minhaVelocidade;
+    public float velocidade = 5f;
+    public float limiteHorizontal = 12f;
+    public AudioClip boing;
+    public Transform transforCamera;
+    public AudioClip ending;
+    public AudioClip backsound;
+    public float delay = 2f;
+    public bool jogoIniciado = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Iniciando a bola com um Delay
+        //Diminuindo o valor do deley
+        delay = delay - Time.deltaTime;
+
+        //Checando se o deley chegou em 0 para então iniciar o jogo
+        if (delay <= 0 && jogoIniciado == false)
+        {
+            //Alterar valor da variável de controle
+            jogoIniciado = true;
+            //Iniciando o jogo
+
+
+            //Tentando gerar um valor aleatório
+            int direcao = Random.Range(0, 4);
+
+            if (direcao == 0)
+            {
+                minhaVelocidade.x = velocidade;
+                minhaVelocidade.y = velocidade;
+            }
+            else if (direcao == 1)
+            {
+                minhaVelocidade.x = -velocidade;
+                minhaVelocidade.y = velocidade;
+            }
+            else if (direcao == 2)
+            {
+                minhaVelocidade.x = -velocidade;
+                minhaVelocidade.y = -velocidade;
+            }
+            else if (direcao == 3)
+            {
+                minhaVelocidade.x = velocidade;
+                minhaVelocidade.y = -velocidade;
+            }
+
+            //Adicionando uma velocidade para a esquerda
+            meuRB.velocity = minhaVelocidade;
+        }
+        
+        //Checando se a bola saiu da tela
+        if(transform.position.x > limiteHorizontal || transform.position.x < -limiteHorizontal)
+        {
+            AudioSource.PlayClipAtPoint(ending, transforCamera.position);
+            //Recarregando a minha scena.
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    //Criando o meu evento de colisão
+    //Evento de colisão 2d
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(boing, transforCamera.position);
+    }
+}
---
+++ Assets/Scripts/RaqueteController.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaqueteController : MonoBehaviour
+{
+    //Criando Vector 3
+    private Vector3 minhaPosicao;
+    private float meuY;
+    public float velocidade = 5f;
+    public float meuLimite = 3.5f;
+
+    //Identificando se eu sou o player 1
+    public bool player1;
+
+    //Variável de checagem IA
+    public bool automatico = false;
+
+    //Pegando a posição da bola
+    public Transform transformBola;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Pegando a posicao inicial da raquete e aplicando à minha posicao
+        minhaPosicao = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Passando o meuY para o eixo Y da minhaPosicao
+        minhaPosicao.y = meuY;
+
+        //Modificar posição da raquete
+        transform.position = minhaPosicao;
+
+        //Velocidade multiplicada pelo deltatime
+        float deltaVelocidade = velocidade * Time.deltaTime;
+
+        //Se o automático não for TRUE
+        if (!automatico)
+        {
+
+            //Pegando a setinha para cima
+            //Se eu aperta a setinha para cima ele vai subir a raquete.
+            //DeltaTime para normalizar frames.
+            //Controlando a raquete como o playerrr 1
+
+            if (player1)
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+
+                    meuY = meuY + deltaVelocidade;
+
+                }
+                //Pegando a setinha para baixo
+                if (Input.GetKey(KeyCode.S))
+                {
+                    //Diminuir o valor do meuY;
+                    meuY = meuY - deltaVelocidade;
+                }
+            }
+            else
+            {
+                //Meu código para colocar ele no automático
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    automatico = true;
+                }
+
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+
+                    meuY = meuY + deltaVelocidade;
+
+                }
+                //Pegando a setinha para baixo
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    //Diminuir o valor do meuY;
+                    meuY = meuY - deltaVelocidade;
+                }
+            }
+        }
+        else
+        {
+
+            //Tirando do automatico
+            //Checando se a setinha para cima ou para baixo foi pressionada
+            if (Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.DownArrow))
+            {
+                automatico = false;
+            }
+
+            //Se a raquete está no automatico
+            //Para acessar funções matemáticas, nós usamos a classe Mathf
+            meuY = Mathf.Lerp(meuY, transformBola.position.y, 0.2f);
+            
+
+        }
+
+        //Impedindo que eu saia por baixo da tela
+        if (meuY < -meuLimite)
+        {
+            meuY = -meuLimite;
+        }
+
+        //Impedindo que eu saia por cima da tela
+        if (meuY > meuLimite)
+        {
+            meuY = meuLimite;
+        }
+
+    }
+}
